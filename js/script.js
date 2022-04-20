93e232ba0f93369a32baa6081587e20381b92031
Architbisht <45563025+Architbisht@users.noreply.github.com>
2022-04-20 13:54:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jordan", "Jason", "Peter", "Frank", "Larry", "Paul", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
